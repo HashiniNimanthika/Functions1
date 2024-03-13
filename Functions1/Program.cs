6bc52ff76c87832bce1637b3d95eaf68bda11170
hashi <hashi@HASHINI>
2024-03-13 18:27:29 +1300

    Add project files.

---
+++ Functions1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Emit;
+
+namespace Functions1
+{
+     class Factorial
+    {
+
+        static int fib(int n)
+        {
+
+          if (n ==5) fib(n - 1);
+
+          return fib(n);
+
+
+
+
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+            Factorial fib = new Factorial();
+            
+        }
+    }
+
+}
+  
+    
+        
